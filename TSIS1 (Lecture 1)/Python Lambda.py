e0387f2393d1eaf691307f2c0cf0b0dbe39bfefb
Almas <73135789+Lumiere2304@users.noreply.github.com>
2021-07-08 12:51:01 +0600

    Delete TSIS1 (Lecture 1) directory

--- TSIS1 (Lecture 1)/Python Lambda.py
+++
@@ -1,9 +0,0 @@
-# Exercise 1:
-x = lambda a, b, c : (a + b) *c
-print(x(5, 3, 3))
-
-# Additional Exercise:
-def my_function(n):
-    return lambda a, b : (a * b) // n
-myseparator = my_function(2)
-print(myseparator(5, 8))
