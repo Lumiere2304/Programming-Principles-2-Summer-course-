4ae7ed1cf25f379073fd7af0dc2dfecc075e3c8a
Almas <73135789+Lumiere2304@users.noreply.github.com>
2021-07-08 12:54:12 +0600

    Lecture 2

---
+++ TSIS1 (Lecture 2)/1732. Find the Highest Altitude.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestAltitude(self, gain):
+        ans = []
+        ans.append(0)
+        for i in range(0, len(gain)):
+            tmp = ans[i] + gain[i]
+            ans.append(tmp)
+
+        return max(ans)
